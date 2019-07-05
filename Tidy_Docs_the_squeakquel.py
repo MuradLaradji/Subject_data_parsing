18cfd5a2304c6e29c4d794a320c1122765cbf2d5
Murad Laradji <muradlaradji@gmail.com>
2019-07-05 17:04:19 -0500

    Almost done! Added changes to how everything works, but now the UID and timestamped licks are working properly. All that is left it to analyze in R.

--- Tidy_Docs_the_squeakquel.py
+++ Tidy_Docs_the_squeakquel.py
@@ -11,20 +11,22 @@ data_location = "taste_data/"
 os.chdir(data_location)  # set working directory to where the data is
 
 # initialize all data containers
-uid = []
-active_licks = []
-inactive_licks = []
+uid = []  # list to hold unique ids
+active_licks = []  # list for active licks
+inactive_licks = []  # list for inactive licks
 
 # looping program to find and list important data
 for filename in os.listdir(os.getcwd()):  # for all files in the current working directory
     file = open(filename, 'rt')
 
+    box = str()
+    subject = str()
+    group = str()
+
     x = file.readlines()  # x is a list where each element is a line in the file
     for line in x:
 
-        box = int()
-        subject = str()
-
+        # Extract box number from file
         if line.startswith('Box:'):
             re1 = '.*?'  # Non-greedy match on filler
             re2 = '(\\d+)'  # Integer Number 1
@@ -33,9 +35,15 @@ for filename in os.listdir(os.getcwd()):  # for all files in the current working
             m = rg.search(line)
             if m:
                 t_box = m.group(1)
-                if t_box.isspace() is False:
-                    print(t_box)
-                    box = str(line)
+                box = str(t_box)
+
+        # Extract group
+        if line.startswith('Group:'):
+            y = line.split()
+            t_group = y[1]
+            group = t_group
+
+        # Extract subject number
         if line.startswith('Subject:'):
             re1 = '.*?'  # Non-greedy match on filler
             re2 = '(?:[a-z][a-z0-9_]*)'  # Uninteresting: var
@@ -46,19 +54,38 @@ for filename in os.listdir(os.getcwd()):  # for all files in the current working
             m = rg.search(line)
             if m:
                 t_subject = m.group(1)
-                if t_subject.isspace() is False:
-                    # print(t_subject)
-                    subject = str(t_subject)
+                subject = t_subject
 
-        t_uid = (box, subject)
-        print(t_uid)
-        uid.append(t_uid)
+        if line.startswith('E:'):
+            ce = 1
 
+        if line.startswith('F:'):
+            ce = 0
+
+        if ce == 1:
+            y = line.split()
+            active_licks.append(y[1:])
+            # print("active", line, '69')
+
+        if ce == 0:
+            y = line.split()
+            inactive_licks.append(y[1:])
+            # print("inactive", line, '74')
+
+        if line.startswith('G:'):
+            ce = 1
+
+    # Create UID
+    t_uid = box + "_" + subject + "_" + group
+    print(t_uid)
+    uid.append(t_uid)
+
+    file.close()
 
 print(uid)
 
-
-
-
-
-
+with open('export_data1.txt', 'w+') as out:
+    out.write(str(active_licks))
+    out.write(str(inactive_licks))
+    out.write(str(uid))
+    out.close()
