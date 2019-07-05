d11f378f38fecc31055df979bac7939977773099
Murad Laradji <muradlaradji@gmail.com>
2019-07-05 12:47:17 -0500

    This is just so nilai can see it

---
+++ If_we_used_isspace.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import re
+
+# ce is a collecting T/F variable, which either allows or prevents the collection of lick data at specific points.
+ce = 0
+
+# working directory
+os.chdir(sys.path[0])  # sys.path[0] returns the location of the script
+data_location = "taste_data/"
+os.chdir(data_location)  # set working directory to where the data is
+
+# initialize all data containers
+uid = []
+active_licks = []
+inactive_licks = []
+
+# looping program to find and list important data
+for filename in os.listdir(os.getcwd()):  # for all files in the current working directory
+    file = open(filename, 'rt')
+
+    x = file.readlines()  # x is a list where each element is a line in the file
+    for line in x:
+
+        box = str()
+        subject = str()
+
+        if line.startswith('Box:'):
+            re1 = '.*?'  # Non-greedy match on filler
+            re2 = '(\\d+)'  # Integer Number 1
+
+            rg = re.compile(re1 + re2, re.IGNORECASE | re.DOTALL)
+            m = rg.search(line)
+            if m:
+                t_box = m.group(1)
+                if t_box.isspace() is False:
+                    print(t_box)
+                    box = str(t_box)
+        if line.startswith('Subject:'):
+            re1 = '.*?'  # Non-greedy match on filler
+            re2 = '(?:[a-z][a-z0-9_]*)'  # Uninteresting: var
+            re3 = '.*?'  # Non-greedy match on filler
+            re4 = '((?:[a-z][a-z0-9_]*))'  # Variable Name 1
+
+            rg = re.compile(re1 + re2 + re3 + re4, re.IGNORECASE | re.DOTALL)
+            m = rg.search(line)
+            if m:
+                t_subject = m.group(1)
+                if t_subject.isspace() is False:
+                    # print(t_subject)
+                    subject = str(t_subject)
+            t_uid = (box, subject)
+            print(t_uid)
+                #uid.append(t_uid)
+                #print(uid)
+
+
+
+
+
+
--- tidy_docs1.py
+++ tidy_docs1.py
@@ -2,10 +2,8 @@ import os
 import sys
 import re
 
-import numpy as np
-import pandas as pd
 
-# ce is the "collecting E" variable, which either allows or prevents the collection of data at specific points.
+# ce is a collecting T/F variable, which either allows or prevents the collection of lick data at specific points.
 ce = 0
 
 # working directory
@@ -18,22 +16,54 @@ uid = []
 active_licks = []
 inactive_licks = []
 
+
 # looping program to find and list important data
 for filename in os.listdir(os.getcwd()):  # for all files in the current working directory
     file = open(filename, 'rt')
 
     x = file.readlines()  # x is a list where each element is a line in the file
     for line in x:
-        if line.startswith('Box:'):
-            re1 = '(\\d+)'  # Integer Number 1
 
-            rg = re.compile(re1, re.IGNORECASE | re.DOTALL)
+        t_uid = []
+        ce = 0
+
+        if line.startswith('Box:'):
+            ce = 1
+
+        if line.startswith('Subject:'):
+            ce = 0
+
+        if ce == 1:
+
+            re1 = '.*?'  # Non-greedy match on filler
+            re2 = '(\\d+)'  # Integer Number 1
+
+            rg = re.compile(re1 + re2, re.IGNORECASE | re.DOTALL)
             m = rg.search(line)
             if m:
-                int1 = m.group(1)
+                box = m.group(1)
+            if len(box) > 0:
+                t_uid.append(box)
 
-            uid.append(int1)
-            print(uid, '33')
+
+            re1 = '.*?'  # Non-greedy match on filler
+            re2 = '(?:[a-z][a-z0-9_]*)'  # Uninteresting: var
+            re3 = '.*?'  # Non-greedy match on filler
+            re4 = '((?:[a-z][a-z0-9_]*))'  # Variable Name 1
+
+            rg = re.compile(re1 + re2 + re3 + re4, re.IGNORECASE | re.DOTALL)
+            m = rg.search(line)
+            if m:
+                subject = m.group(1)
+
+            if len(subject) > 0:
+                t_uid.append(subject)
+
+        uid.append(t_uid)
+        # print(uid, '56')
+
+        # ce is a collecting T/F variable, which either allows or prevents the collection of lick data.
+        ce = 0
 
         if line.startswith('E:'):
             ce = 1
@@ -44,33 +74,34 @@ for filename in os.listdir(os.getcwd()):  # for all files in the current working
             y = line.split()
             assert isinstance(y, object)
             active_licks.append(y[1:])
-            print("active", line, '38')
+            print("active", line, '67')
 
         if ce == 0:
             y = line.split()
             assert isinstance(y, object)
             inactive_licks.append(y[1:])
-            print("inactive", line, '44')
+            print("inactive", line, '73')
 
         if line.startswith('G:'):
-            y = 10
-
+            ce == 1
     file.close()
 
+
+
 # writes the strings to a new csv document
-with open('C:\\Users\\murad\\PycharmProjects\\Subject_data_parsing\\venv\\export_data1.csv', 'w+') as out:
+with open('C:\\Users\\murad\\PycharmProjects\\Subject_data_parsing\\venv\\export_data1.txt', 'w+') as out:
     out.write(str(active_licks))
     out.write(str(inactive_licks))
     out.write(str(uid))
     out.close()
 
 # combine your variables into a pandas data frame
-Subject_Data = pd.DataFrame(list(map(list, zip(uid, active_licks, inactive_licks))))
-Subject_Data.columns = ['UID', 'active licks', 'inactive licks']
+#Subject_Data = pd.DataFrame(list(map(list, zip(uid, active_licks, inactive_licks))))
+#Subject_Data.columns = ['UID', 'active licks', 'inactive licks']
 
-print(Subject_Data)
+#print(Subject_Data)
 
 # export the data into a csv file
-Subject_Data.to_csv("C:\\Users\\murad\\PycharmProjects\\Subject_data_parsing\\export_data.csv", index=True)
+
 
 # write a copy to the R directory (only applicable to this computer! Change the path for your own!)
