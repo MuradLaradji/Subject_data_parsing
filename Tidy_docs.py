f39d6fecbc88c82046b8fc634ca742fc09591195
Murad Laradji <muradlaradji@gmail.com>
2019-08-16 10:23:58 -0500

    Finished Parsing Project. This parsing script combined with the analytical script by

--- Tidy_docs.py
+++ Tidy_docs.py
@@ -3,26 +3,25 @@ import sys
 import re
 import csv
 
+
 # ce is a collecting T/F variable, which either allows or prevents the collection of lick data at specific points.
 ce = -1
 
 # working directory
 os.chdir(sys.path[0])  # sys.path[0] returns the location of the script
-data_location = "taste_data/"
+data_location = "taste_data/Py_read"
 os.chdir(data_location)  # set working directory to where the data is
-
-# initialize all data containers
-uid = []  # list to hold unique ids
-active_licks = []  # list for active licks
-inactive_licks = []  # list for inactive licks
 
 # looping program to find and list important data
 for filename in os.listdir(os.getcwd()):  # for all files in the current working directory
-    file = open(filename, 'rt')
+    file = open(filename, "rt", encoding="utf8")
 
-    box = str()
-    subject = str()
-    group = str()
+    uid = str()  # string for uid
+    active_licks = []  # list for active licks
+    inactive_licks = []  # list for inactive licks
+    box = str()  # string for box number
+    subject = str()  # string for subject number
+    group = str()  # string for group number
 
     x = file.readlines()  # x is a list where each element is a line in the file
     for line in x:
@@ -66,40 +65,33 @@ for filename in os.listdir(os.getcwd()):  # for all files in the current working
         if ce == 1:
             y = line.split()
             active_licks.extend(y[1:])
-           # print("active",)
+        # print("active",)
 
         if ce == 0:
             y = line.split()
             inactive_licks.extend(y[1:])
-           # print("inactive")
+        # print("inactive")
 
         if line.startswith('G:'):
             ce = -1
 
-    # Create UID
+    # Create UID and file names
     t_uid = box + "_" + subject + "_" + group
-    print(t_uid)
-    uid.append(t_uid)
+    file_name_inactive = t_uid + '_inactive.csv'  # string for naming tidier data files
+    file_name_active = t_uid + '_active.csv'  # string for naming tidier data files
+
+    # uid.append(t_uid)
+
+    with open(file_name_inactive, 'w+', newline='') as file:
+        wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+        wr.writerow(inactive_licks)
+        file.close()
+
+    with open(file_name_active, 'w+', newline='') as file:
+        wr = csv.writer(file, quoting=csv.QUOTE_ALL)
+        wr.writerow(active_licks)
+        file.close()
 
     file.close()
 
-print(active_licks, sep=", ")
-
-
-with open('uid_data.csv', 'w+', newline= '') as file:
-    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
-    wr.writerow(uid)
-    file.close()
-
-
-with open('inactive_lick_data.csv', 'w+', newline='') as file:
-    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
-    wr.writerow(inactive_licks)
-    file.close()
-
-
-with open('active_lick_data.tab', 'w+', newline='') as file:
-    wr = csv.writer(file, quoting=csv.QUOTE_ALL)
-    wr.writerow(active_licks)
-    file.close()
-
+print(inactive_licks)
